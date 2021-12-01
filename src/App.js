0871bb98fa49a4e55ec8d67925fddc9bf7bcc09d
Sharu <jsrshark110@gmail.com>
2021-12-01 00:10:12 -0800

    remot repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import { Switch, Route, Link } from 'react-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>
-      console.log('haha');
+    <div className="app">
+      <div className="navbar">
+
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
